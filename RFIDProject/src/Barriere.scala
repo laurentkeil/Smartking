b248a689a5caa9f3130d31a73d66ce038a3a736f
sniperx21x <laurent_0306@hotmail.com>
2015-03-21 17:35:23 +0100

    interface user RFID

--- RFIDProject/src/Barriere.scala
+++ RFIDProject/src/Barriere.scala
@@ -1,13 +1,6 @@
-
-
-
-
-/*
- * Copyright 2007 Phidgets Inc.  All rights reserved.
- */
-
-import java.awt.Button;
-import java.awt.Frame;
+/** 
+ * @author Jason 
+ */ 
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.io.IOException;
@@ -27,86 +20,59 @@ class Barriere {
 		 * Régler les paramètres du servo motor
 		 * Set Min Position = 0; Set Max Position=100
 		 */
-		
-		this.servo.addAttachListener(new AttachListener() {
+		servo.addAttachListener(new AttachListener() {
 			def attached(ae:AttachEvent ) = {
-				System.out.println("attachment of " + ae);
-				//if(this.open==true){this.ouverture();}
-				//if(this.close==true){}
-				System.out.println("L'erreur est : " + ae.getClass());
+				println("attachment of " + ae);
         config();
 			}
 		});
-		this.servo.addDetachListener(new DetachListener() {
+		servo.addDetachListener(new DetachListener() {
 			def  detached(ae:DetachEvent) {
-				System.out.println("detachment of " + ae);
-				System.out.println("Veuillez le rebranchez");
+				println("detachment of " + ae);
 				try {
 					servo.setPosition(0, 20.0);
 				} catch {
-					// TODO Auto-generated catch block
-          case e : PhidgetException =>{ e.printStackTrace();
-					System.out.println("Problème quand on le branche");}
+          case e : PhidgetException => println("Problème quand on le branche");
 				}
 			}
 		});
-		this.servo.addErrorListener(new ErrorListener() {
+		servo.addErrorListener(new ErrorListener() {
 			def  error(ee:ErrorEvent ) {
-				System.out.println("error event for " + ee);
+				println("error event for " + ee);
 			}
 		});
-		this.servo.addServoPositionChangeListener(new ServoPositionChangeListener()
-		{
-			def servoPositionChanged(oe:ServoPositionChangeEvent)
-			{
-				System.out.println(oe);
+		servo.addServoPositionChangeListener(new ServoPositionChangeListener() {
+			def servoPositionChanged(oe:ServoPositionChangeEvent) {
+				println(oe);
 			}
 		});
-    
     config();
-		
 	}
 	
-  def config(){
-    
-    System.out.println("Configuration du servo motor");
-    /*System.out.println("Serial: " + servo.getSerialNumber());
-    System.out.println("Servos: " + servo.getMotorCount());*/
-    System.out.println("waiting for AdvancedServo attachment...");
+  def config() {
+    println("Configuration du servo motor");
+    println("waiting for AdvancedServo attachment...");
     servo.openAny();
     servo.waitForAttachment();
-    //System.out.println("Ligne 70 : "+ servo.getVelocity(0) + " - Position : " + servo.getPosition(0));
-    
     servo.openAny();
     servo.setPosition(0, 10.0);
-    //System.out.println("Renew 70 : "+ servo.getVelocity(0) + " - Position : " + servo.getPosition(0));
-
-    //this.speed()
-    this.servo.setEngaged(0, true);
+    servo.setEngaged(0, true);
   }
   
-  
   def ouverture():Unit ={
-	
-		System.out.println("Ouverture barrière...");
-	    	this.servo.setPosition(0, 120.0);
-      
-		System.out.println("Barrière ouverte");
+		println("Ouverture barrière...");
+	  servo.setPosition(0, 120.0);
+		println("Barrière ouverte");
 	}	
 	
 	def  fermeture() = {
-		
-		System.out.println("Fermeture barrière...");
-	    	this.servo.setPosition(0, 20.0);	
-       
-		System.out.println("Barrière fermée");
+		println("Fermeture barrière...");
+	  servo.setPosition(0, 20.0);	
+		println("Barrière fermée");
 	}
 	
-	def  close_system() ={
-		
-		this.servo.setEngaged(0, false);
-		//this.servo.close();
-		
+	def  close_system() = {
+		servo.setEngaged(0, false);
 	}
 }
 
--- RFIDProject/src/RFID.scala
+++ RFIDProject/src/RFID.scala
@@ -5,114 +5,325 @@ import com.phidgets.event._
 import scalaj.http._
 import java.io.InputStreamReader
 import org.json._
- 
+import javax.swing._
+import java.awt.event._
+import java.awt._
+  
 /** 
  * @author laurent 
  */ 
-object RFID {
+object RFID extends JFrame {
   
-  def main(args:Array[String]) {
-      val rfid = new RFIDPhidget()
-      val barriere = new Barriere()
-      barriere.Barriere()
+        val rfid = new RFIDPhidget()
+        //val barriere = new Barriere()    
+        var action = "no"
+  
+        val container = new JPanel()
+        val center = new JPanel()
+        val south = new JPanel()
+        val top = new JPanel();
+        val readPan = new JPanel();
+        val userPan = new JPanel();
+        val write = new JButton("Inscription")
+        val update = new JButton("Mise à jour")
+        val in = new JButton("Entrée parking")
+        val out = new JButton("Sortie Parking")
+        val open = new JButton("Ouvrir barrière")
+        val close = new JButton("Fermer barrière")
+        val choix = new JLabel("Choix de l'action")
+        val combo = new JComboBox(new Array[String](0))
+        val tagLabel = new JLabel();
+        val tagLu = new JTextField();
+        val userNomLabel = new JLabel();
+        val userNom = new JTextField();
+        val userPrenomLabel = new JLabel();
+        val userPrenom = new JTextField();
+        val userMailLabel = new JLabel();
+        val userMail = new JTextField();
+        val userMdpLabel = new JLabel();
+        val userMdp = new JTextField();
+        val client = new JCheckBox("Client");
       
-      rfid.addAttachListener(new AttachListener() {
-        def attached(ae : AttachEvent ) 
-        {
-          try
-          {
-            (ae.getSource() match { 
-              case aeRFID : RFIDPhidget => aeRFID
-            }).setAntennaOn(true); 
-            
-            (ae.getSource() match {
-              case aeRFID : RFIDPhidget => aeRFID
-            }).setLEDOn(true);
-          }
-          catch { 
-            case exc : PhidgetException => println(exc)
-          }
-          println("attachment 1 of " + ae);
-        }
-      });
-      rfid.addDetachListener(new DetachListener() {
-        def detached(ae : DetachEvent ) {
-          println("detachment  1 of " + ae);
-        } 
-      });
-      rfid.addErrorListener(new ErrorListener() {
-        def error(ee : ErrorEvent) {
-          println("error event for " + ee);
-        }
-      });
+    /** Creates new form RFID */
+    def RFID() {
+        initComponents()
+    }
+    
+    def initComponents() {
       
-      rfid.openAny();
-      println("waiting for RFID attachment...");
-      rfid.waitForAttachment(1000);
-      
-      val actionStr = Http.get("http://smarking.azurewebsites.net/api/global/rfid").asString
-      val action = new JSONObject(actionStr).getString("value")
-      action match {
-        case ("in" | "out") => {
-              rfid.addTagGainListener(new TagGainListener()
-              { 
-                def tagGained(oe : TagGainEvent)
-                {
-                  println("\nTag Gained: " + oe.getValue());
-                  //recherche le tag du user en BD
-                  val responsePost = Http.post("http://smarking.azurewebsites.net/api/FlowUsers").params("action" ->action).params("idTag" -> oe.getValue).asString        
-                  println(responsePost)
-                  if (responsePost != "\"NotFound\"" && responsePost != "\"AccessDenied\"") {                    
-                     rfid.setOutputState(1, true) //vert si le tag est présent en BD
-                     barriere.ouverture()
-                     Thread.sleep(2000)
-                     barriere.fermeture()
-                  } else {
-                     rfid.setOutputState(0, true) //rouge si le tag est présent en BD
-                  }
-                }
-              });
-              rfid.addTagLossListener(new TagLossListener()
-              {
-                def tagLost(oe : TagLossEvent)
-                {
-                  println("Tag Loss : " + oe.getValue());
-                  rfid.setOutputState(0, false)
-                  rfid.setOutputState(1, false)
-                }
-              });
-              rfid.addOutputChangeListener(new OutputChangeListener()
-              {
-                def outputChanged(oe : OutputChangeEvent)
-                {
-                  println(oe.getIndex + " change to " + oe.getState);
-                }
-              });
-        }
-        case "write" => {      
-              //write a tag:
-              try {
-                  val uuid = UUID.randomUUID();
-                  val time = System.currentTimeMillis()/1000;
-                  val tag = (time.toString() ++ uuid.toString.replace("-", "").substring(9,23))
-                  rfid.write(tag, RFIDPhidget.PHIDGET_RFID_PROTOCOL_PHIDGETS, false);  //écrit sur la carte
-                  println("\nWrite Tag : " + tag);
-                  //entre le tag du user en BD
-                  val responsePost = Http.post("http://smarking.azurewebsites.net/api/users").params("idTag" -> tag).asString        
-                  println(responsePost)
-                  rfid.setOutputState(1, true) //ecriture : vert si tag a bien été écrit en BD
-                  Thread.sleep(1000)
-                  rfid.setOutputState(1, false)
-              } catch {
-                  case exc : PhidgetException => println(exc)
-              }
-        }
-        case _ => println("Erreur.")
+        //Définit un titre pour notre fenêtre
+        setTitle("RFID SmartKing")
+        //Définit sa taille : 400 pixels de large et 100 pixels de haut
+        setSize(350, 550)
+        //Nous demandons maintenant à notre objet de se positionner au centre
+        setLocationRelativeTo(null)
+        //Termine le processus lorsqu'on clique sur la croix rouge
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+        setResizable(false)
+
+        container.setBackground(Color.GRAY)
+        container.setLayout(new BorderLayout())
+        
+        combo.addItem("Scan")
+        combo.addItem("Inscription")  
+        combo.addActionListener(new FormListener())
+        top.add(choix)
+        top.add(combo)    
+        container.add(top, BorderLayout.NORTH)
+         
+        userNomLabel.setText("Nom : ")
+        userNom.setEditable(false)
+        userNom.setPreferredSize(new Dimension(300, 30))
+        userPrenomLabel.setText("Prénom : ")
+        userPrenom.setEditable(false)
+        userPrenom.setPreferredSize(new Dimension(300, 30))
+        userMailLabel.setText("E-mail : ")
+        userMail.setEditable(false)
+        userMail.setPreferredSize(new Dimension(300, 30))
+        userMdpLabel.setText("Mot de passe : ")
+        userMdp.setEditable(false)
+        userMdp.setPreferredSize(new Dimension(300, 30))
+        userPan.setBorder(BorderFactory.createTitledBorder("Informations utilisateur"))
+        userPan.setBackground(Color.WHITE)
+        userPan.add(userNomLabel)
+        userPan.add(userNom)
+        userPan.add(userPrenomLabel)
+        userPan.add(userPrenom)
+        userPan.add(userMailLabel)
+        userPan.add(userMail)
+        userPan.add(userMdpLabel)
+        userPan.add(userMdp)
+        client.setEnabled(false)
+        userPan.add(client)
+        
+        tagLabel.setText("Tag : ")
+        tagLu.setEditable(false)
+        tagLu.setPreferredSize(new Dimension(300, 30))
+        readPan.setBorder(BorderFactory.createTitledBorder("Tag lu"))
+        readPan.setPreferredSize(new Dimension(300, 100))
+        readPan.setBackground(Color.WHITE)
+        readPan.add(tagLabel)
+        readPan.add(tagLu)
+        
+        center.setLayout(new BorderLayout())
+        center.setBackground(Color.WHITE)
+        center.add(readPan, BorderLayout.NORTH)
+        center.add(userPan, BorderLayout.CENTER)
+        container.add(center, BorderLayout.CENTER)
+        
+        south.setPreferredSize(new Dimension(300, 70))
+        in.addActionListener(new inListener())
+        south.add(in)
+        out.addActionListener(new outListener())  
+        south.add(out)
+        open.addActionListener(new openListener()) 
+        south.add(open)
+        close.addActionListener(new closeListener())
+        south.add(close)
+        container.add(south, BorderLayout.SOUTH)
+    
+        setContentPane(container)  
+        
+        //Et enfin, la rendre visible        
+        setVisible(true)
+    }
+    
+     //Classe écoutant notre bouton
+    class openListener extends ActionListener {
+       def actionPerformed(arg0 : ActionEvent) {
+         //barriere.ouverture()
       }
-   
-      println("Outputting events.  Input to stop.");
-      System.in.read();
-      print("closing...");
-      rfid.close();
-  }
+    }
+     //Classe écoutant notre bouton
+    class closeListener extends ActionListener {
+       def actionPerformed(arg0 : ActionEvent) {
+         //barriere.fermeture()
+      }
+    }
+     //Classe écoutant notre bouton
+    class inListener extends ActionListener {
+       def actionPerformed(arg0 : ActionEvent) {
+         action = "in"
+      }
+    }
+     //Classe écoutant notre bouton
+    class outListener extends ActionListener {
+       def actionPerformed(arg0 : ActionEvent) {
+         action = "out"
+      }
+    }
+     //Classe écoutant notre bouton
+    class updateListener extends ActionListener {
+       def actionPerformed(arg0 : ActionEvent) {
+         action = "update"
+         //write a tag:
+                try {
+                    val tag = tagLu.getText
+                    rfid.write(tag, RFIDPhidget.PHIDGET_RFID_PROTOCOL_PHIDGETS, false);  //écrit sur la carte
+                    println("\nWrite Tag : " + tag);
+                    //entre le tag du user en BD
+                    val responsePost = Http.post("http://smarking.azurewebsites.net/api/users").params(Map(("idTag", tag))).asString        
+                    println(responsePost)
+                    rfid.setOutputState(1, true) //ecriture : vert si tag a bien été écrit en BD
+                    JOptionPane.showMessageDialog(null, "Les informations ont été mise à jour", "Mis à jour", JOptionPane.INFORMATION_MESSAGE);
+                    Thread.sleep(1000)
+                    rfid.setOutputState(1, false)
+                } catch {
+                    case exc : PhidgetException => println(exc)
+                    rfid.setOutputState(0, true)
+                    JOptionPane.showMessageDialog(null, "Les informations n'ont pas pu être mise à jour", "Mis à jour", JOptionPane.ERROR_MESSAGE);                   
+                    Thread.sleep(1000)
+                    rfid.setOutputState(0, false)
+                }
+      }
+    }
+     //Classe écoutant notre bouton
+    class writeListener extends ActionListener {
+       def actionPerformed(arg0 : ActionEvent) {
+         action = "write"
+          //write a tag:
+                try {
+                    val uuid = UUID.randomUUID();
+                    val time = System.currentTimeMillis()/1000;
+                    val tag = (time.toString() ++ uuid.toString.replace("-", "").substring(9,23))
+                    rfid.write(tag, RFIDPhidget.PHIDGET_RFID_PROTOCOL_PHIDGETS, false);  //écrit sur la carte
+                    println("\nWrite Tag : " + tag);
+                    //entre le tag du user en BD
+                    val responsePost = Http.post("http://smarking.azurewebsites.net/api/users").params(Map(("idTag", tag))).asString        
+                    println(responsePost)
+                    rfid.setOutputState(1, true) //ecriture : vert si tag a bien été écrit en BD
+                    //Boîte du message d'information
+                    JOptionPane.showMessageDialog(null, "L'utilisateur est bien inscrit", "Inscription", JOptionPane.INFORMATION_MESSAGE);
+                    Thread.sleep(1000)
+                    rfid.setOutputState(1, false)
+                } catch {
+                    case exc : PhidgetException => println(exc) 
+                    rfid.setOutputState(0, true)
+                    JOptionPane.showMessageDialog(null, "L'utilisateur n'a pas été inscrit", "Inscription", JOptionPane.ERROR_MESSAGE);                      
+                    Thread.sleep(1000)
+                    rfid.setOutputState(0, false)
+                }
+      }
+    }
+     
+    // Changement de formulaire
+    class FormListener extends ActionListener {
+      def actionPerformed(e : ActionEvent) {
+        if(combo.getSelectedItem() == "Inscription") {
+          south.removeAll()
+          south.revalidate()
+          south.repaint()
+          write.addActionListener(new writeListener())
+          south.add(write)
+          update.addActionListener(new updateListener())
+          south.add(update)
+          userNom.setEditable(true)
+          userPrenom.setEditable(true)
+          userMail.setEditable(true)
+          userMdp.setEditable(true)
+        } else {
+          south.removeAll()
+          south.revalidate()
+          south.repaint()
+          in.addActionListener(new inListener())
+          south.add(in)
+          out.addActionListener(new outListener())  
+          south.add(out)
+          open.addActionListener(new openListener()) 
+          south.add(open)
+          close.addActionListener(new closeListener())
+          south.add(close)
+          userNom.setEditable(false)
+          userPrenom.setEditable(false)
+          userMail.setEditable(false)
+          userMdp.setEditable(false)
+        }
+      }  
+    } 
+    
+    def main(args:Array[String]) {
+        //barriere.Barriere()
+        
+        RFID()
+        
+        rfid.addAttachListener(new AttachListener() {
+          def attached(ae : AttachEvent ) 
+          {
+            try
+            {
+              (ae.getSource() match { 
+                case aeRFID : RFIDPhidget => aeRFID
+              }).setAntennaOn(true); 
+              
+              (ae.getSource() match {
+                case aeRFID : RFIDPhidget => aeRFID
+              }).setLEDOn(true);
+            }
+            catch { 
+              case exc : PhidgetException => println(exc)
+            }
+            println("attachment 1 of " + ae);
+          }
+        });
+        rfid.addDetachListener(new DetachListener() {
+          def detached(ae : DetachEvent ) {
+            println("detachment  1 of " + ae);
+          } 
+        });
+        rfid.addErrorListener(new ErrorListener() {
+          def error(ee : ErrorEvent) {
+            println("error event for " + ee);
+          }
+        });
+        rfid.addOutputChangeListener(new OutputChangeListener() {
+                  def outputChanged(oe : OutputChangeEvent)
+                  {
+                    println(oe.getIndex + " change to " + oe.getState);
+                  }
+        });
+        
+        rfid.openAny();
+        println("waiting for RFID attachment...");
+        rfid.waitForAttachment(1000);
+        
+        rfid.addTagGainListener(new TagGainListener()
+                { 
+                  def tagGained(oe : TagGainEvent)
+                  {
+                    println("\nTag Gained: " + oe.getValue());
+                    tagLu.setText(oe.getValue())
+                    //recherche le tag du user en BD
+                    if(action == "in" || action == "out") {
+                      val responsePost = Http.post("http://smarking.azurewebsites.net/api/FlowUsers").params(Map(("action", action),("idTag", oe.getValue))).asString        
+                      println(responsePost)
+                      if (responsePost != "\"NotFound\"" && responsePost != "\"AccessDenied\"") {    
+                         client.setSelected(true)
+                         rfid.setOutputState(1, true) //vert si le tag est présent en BD
+                         //barriere.ouverture()
+                         //Boîte du message d'information
+                         JOptionPane.showMessageDialog(null, "L'utilisateur est bien passé", "Passage", JOptionPane.INFORMATION_MESSAGE);
+                         Thread.sleep(2000)
+                         //barriere.fermeture()
+                      } else {  
+                         rfid.setOutputState(0, true) //rouge si le tag est présent en BD
+                         JOptionPane.showMessageDialog(null, "L'utilisateur n'a pas pu passer", "Passage", JOptionPane.ERROR_MESSAGE);
+                         if(responsePost != "\"NotFound\"") client.setSelected(true) else client.setSelected(false)
+                      }
+                    }
+                  }
+                });
+        rfid.addTagLossListener(new TagLossListener()
+                {
+                  def tagLost(oe : TagLossEvent)
+                  {
+                    println("Tag Loss : " + oe.getValue());
+                    rfid.setOutputState(0, false)
+                    rfid.setOutputState(1, false)
+                  }
+                });
+     
+        println("Outputting events.  Input to stop.");
+        System.in.read();
+        rfid.close();
+    }
 }
