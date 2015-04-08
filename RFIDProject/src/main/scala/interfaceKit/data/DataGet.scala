f8f455cf77aa0cd3dfb3bd1960494189b0642354
svonhaus <vonhaus.steven@outlook.com>
2015-04-08 09:38:21 +0200

    IntelliJ
    IntelliJ

---
+++ RFIDProject/src/main/scala/interfaceKit/data/DataGet.scala
@@ -0,0 +1,42 @@
+package interfaceKit.data
+
+import org.json._
+import scalaj.http._
+import model._
+
+object DataGet 
+{
+  def found(tag : String) : Option[Person] = 
+  {
+    println("test2")
+
+    val url = "http://smarking.azurewebsites.net/api/users/" + tag
+    val responseGet = Http.get(url).asString
+
+    println("test3")
+
+    if (responseGet != "\"TagNotFound\"") 
+    {
+      val temp = new JSONObject(responseGet)
+      Some(new Person(temp.get("id").toString, temp.get("firstname").toString, temp.get("lastname").toString, temp.get("mail").toString))
+    } 
+    else None
+  }
+  
+  def searchTagUser (tag : String, action : String) : Boolean = 
+  {
+    val responseGet = Http.get("http://smarking.azurewebsites.net/api/Tags/"+ action +"/" + tag).asString
+    
+    if (responseGet == "\"Ok\"") 
+        true
+    else 
+        false
+  }
+  
+  def foundAction () : String = 
+  {
+    val actionStr = Http.get("http://smarking.azurewebsites.net/api/global/rfid").asString
+    new JSONObject(actionStr).getString("value")
+  }
+  
+}
